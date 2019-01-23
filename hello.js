4f175076e7e1d8062ff7b594970cdfe9ad1a8075
Nathan Fiscus <nfiscus@una.edu>
2019-01-23 16:46:22 +0000

    Daily commit for 1/23/19

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
-console.log("Hello, we are here!");
+const http = require('http'); //how we require modules
+
+http.createServer(function(request, response){
+    response.writeHead(200); //status code in header
+    response.write("Hello, this is dog."); //response body
+    response.end(); //close the connection
+}).listen(8080); //listen for connections on this port
+
+console.log('Listening on port 8080...');
