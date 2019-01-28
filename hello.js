0df3143089cc13f6b713ca7e50a00c3f4ef8c661
Nathan Fiscus <nfiscus@una.edu>
2019-01-28 16:38:21 +0000

    Daily 1/28/19

--- hello.js
+++ hello.js
@@ -2,8 +2,10 @@ const http = require('http'); //how we require modules
 
 http.createServer(function(request, response){
     response.writeHead(200); //status code in header
-    response.write("Hello, this is dog."); //response body
-    response.end(); //close the connection
+    response.write("<html><body><p>Dog is running.</p>"); //response body
+    setTimeout(function(){
+        response.write("<p>Dog is done.</p></body></html>");
+        response.end(); //close the connection
+    }, 5000);
 }).listen(8080); //listen for connections on this port
-
 console.log('Listening on port 8080...');
