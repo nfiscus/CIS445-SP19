9b2dbbe1991429acdb3b6497755fe92a0c46d08f
Nathan Fiscus <nfiscus@una.edu>
2019-01-30 17:27:39 +0000

    Classwork for 1/30/19

--- hello.js
+++ hello.js
@@ -1,11 +1,11 @@
 const http = require('http'); //how we require modules
 
 http.createServer(function(request, response){
-    response.writeHead(200); //status code in header
+    response.writeHead(200); //status code in header; successfully connected
     response.write("<html><body><p>Dog is running.</p>"); //response body
     setTimeout(function(){
-        response.write("<p>Dog is done.</p></body></html>");
-        response.end(); //close the connection
-    }, 5000);
+       // response.write("<html><body><p>Dog is done.</p></body></html>");
+        response.end('Dog is done.'); //close the connection
+    }, 5000); //wait 5 seconds before executing, "Dog is done."
 }).listen(8080); //listen for connections on this port
 console.log('Listening on port 8080...');
