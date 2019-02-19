c5b1a14ec99f99811a7a55a74063d4f436fa5f47
Nathan Fiscus <nfiscus@una.edu>
2019-02-19 21:33:35 +0000

    Finished Restful API calls

--- hello.js
+++ hello.js
@@ -1,11 +1,40 @@
 const http = require('http'); //how we require modules
+const express = require('express');
+const app = express();
 
-http.createServer(function(request, response){
+app.get('/', function(request, response){
+   response.writeHead(200);
+   response.write('<html><body><p>Please specify an animal in the URL path.</p></body></html>');
+   response.end();
+});
+
+app.get('/:animal', function(request, response){
+    const animal = request.params.animal;
     response.writeHead(200); //status code in header; successfully connected
-    response.write("<html><body><p>Dog is running.</p>"); //response body
-    setTimeout(function(){
-       // response.write("<html><body><p>Dog is done.</p></body></html>");
-        response.end('Dog is done.'); //close the connection
+    response.write("<html><body><p>" + animal + " is running.</p>" + "<p>I see you are calling: " + animal + "</p>"); //response body
+    response.end(animal + ' is done.');
+});
+
+app.delete('/:animal', function(request, response){
+    const animal = request.params.animal;
+    response.writeHead(200); //status code in header; successfully connected
+    response.write("<html><body><p>You have terminated " + animal + ".</p></body></html>"); //response body
+    response.end();
+});
+
+app.listen(8080, function(){
+    console.log('express is now running on port 8080...');
+});
+
+/*http.createServer(function(request, response){
+    let animal = 'not sure';
+    if(request.url == '/cat/' || request.url == '/cat') animal = "cat";
+    if(request.url == '/dog/' || request.url == '/dog') animal = "dog";
+     //close the connection
     }, 5000); //wait 5 seconds before executing, "Dog is done."
 }).listen(8080); //listen for connections on this port
 console.log('Listening on port 8080...');
+*/
+
+//http route = path, method
+//rest = CRUD; get, post, update, delete
